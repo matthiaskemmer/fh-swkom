7e809518812d2ef08e3e6e73af8c15ff77d022dd
Matthias Kemmer <if22b032@technikum-wien.at>
2024-09-23 16:55:29 +0200

    Add nginx & web-ui

--- web-ui/Program.cs
+++ web-ui/Program.cs
@@ -5,9 +5,19 @@ namespace web_ui
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Fuege statische Dateien hinzu
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            // Aktiviere die Verwendung von statischen Dateien
+            app.UseStaticFiles();
+
+            // Aktiviere den Routing-Support
+            app.UseRouting();
+
+            app.MapGet("/home", () => Results.Redirect("/index.html")); // Weiterleitung auf die Hauptseite
 
             app.Run();
         }
