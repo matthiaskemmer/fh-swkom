a1a3ceaf0474a4a026e09789496f244c76732243
Matthias Kemmer <if22b032@technikum-wien.at>
2024-09-28 16:40:13 +0200

    Add fetch documents from rest-api

---
+++ web-ui/wwwroot/js/home.js
@@ -0,0 +1,24 @@
+﻿const apiUrl = 'http://localhost:8081/documents';
+
+// Fetch documents
+function fetchAllDocuments() {
+    console.log('Fetching Document items...');
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const documentList = document.getElementById('documentList');
+            documentList.innerHTML = ''; // Clear the list before appending new items
+            data.forEach(docElement => {
+                const doc = document.createElement("div");
+                doc.className = "card mb-2 w-25"
+                doc.innerHTML = `
+                    <div class="card-body"><p class="card-text">Id: ${docElement.id} | Name: ${docElement.title}</p></div>
+                `;
+                documentList.appendChild(doc);
+            });
+        })
+        .catch(error => console.error('Error while fetching documents...', error));
+}
+
+// Load items on page load
+document.addEventListener('DOMContentLoaded', fetchAllDocuments);
